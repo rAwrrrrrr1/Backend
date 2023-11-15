df0c9981ee3b043c9dabc40eb0175e93133f3e28
rAwrrrrrr1 <bet14a@gmail.com>
2023-11-15 21:51:42 +0700

    Login,Logout,Register

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $registrationData = $request->all();
+
+        $validate = Validator::make($registrationData, [
+            'email' => 'required|email:rfc,dns|unique:users',
+            'password' => ['required'],
+            'nama' => 'required',
+            'telepon' => 'required|regex:/^08[0-9]/',
+        ]);
+
+        if ($validate->fails())
+            return response(['message' => $validate->errors()], 400);
+
+        $registrationData['password'] = bcrypt($request->password);
+
+        $user = User::create($registrationData);
+
+        return response([
+            'message' => 'Register Success',
+            'user' => $user
+        ], 200);
+    }
+
+    public function login(Request $request)
+    {
+        $validatedData = $request->validate([
+            'email' => 'required|email:rfc,dns',
+            'password' => 'required',
+        ]);
+
+        $user = User::where('email', $validatedData['email'])->first();
+
+        if ($user && Hash::check($validatedData['password'], $user->password)) {
+            Auth::login($user);
+            $token = $user->createToken('Authentication Token')->accessToken;
+            return response()->json([
+                'success' => true,
+                'message' => 'Authenticated',
+                'user' => $user,
+                'token_type' => 'Bearer',
+                'access_token' => $token
+            ]);
+        } else {
+            return response(['message' => 'Invalid Credentials'], 401);
+        }
+    }
+
+    public function logout(){
+        $user = Auth::user()->token();
+        $user->revoke();
+        return response()->json([
+            'success' => true,
+            'message'=>'Logout Success'
+        ],200);
+    }
+}
